c69908c6641533dde6350b749f4c83f4eaa45dc0
soloproyectos <gonzalo.chumillas@gmail.com>
2018-01-30 17:44:47 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment as env } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  sections = [
+    {
+      title: 'Content Management System',
+      description: 'Blah blah blah, blah blah blah',
+      snapshots: [
+        { src: 'data/images/axistools1.png', title: 'Listado de registros' },
+        { src: 'data/images/axistools2.png', title: 'Editor de registros' },
+        { src: 'data/images/axistools3.png', title: 'Editor de imágenes' },
+        { src: 'data/images/axistools4.png', title: 'Editor de textos' },
+        { src: 'data/images/axistools5.png', title: 'Editor de vídeos' }
+      ]
+    }
+  ];
+
+  getFullPath(src: string) {
+    return [env.apiUrl, src].join('/');
+  }
 }
